6bf650e0dd0807274fcfb10b9a46d640efbe3d9e
ashishb012 <ashishb0128@gmail.com>
2026-06-04 17:44:00 +0530

    minor fixes

--- app/src/main/java/com/ab/datawatch/ui/components/AppUsageItem.kt
+++ app/src/main/java/com/ab/datawatch/ui/components/AppUsageItem.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Icon
